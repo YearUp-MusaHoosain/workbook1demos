35505a2c4de186f02923968db50b1e81eba6461b
Musa Hoosain <musahoosain@gmail.com>
2024-09-26 15:57:25 -0400

    add TV calculation

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -4,5 +4,28 @@ public class Demo1 {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        char status = 'm';
+        int identifier = 1;
+        String name = "Matt";
+        String greeting = "Hello " + name;
+//        String greeting = "Hello " + "Matt";
+//        String greeting = "Hello Matt"
+
+        int numberOfDaysPerWeekWatchesTV = 7;
+        int minutesPerDayOnDayWatchedOnAverage = 120;
+
+        int minutesPerWeek = numberOfDaysPerWeekWatchesTV * minutesPerDayOnDayWatchedOnAverage;
+        int minutesPerYear = minutesPerWeek * 52;
+
+        int hoursPerYear = minutesPerYear / 60;
+
+        int hoursInWorkWeek = 35;
+
+        int workWeeksPerYearWatchingTV = hoursPerYear / hoursInWorkWeek;
+
+        System.out.println("If " + name + " did not watch TV, it would be like a " + workWeeksPerYearWatchingTV + " week vacation every year");
+
+        System.out.println(greeting);
     }
 }
